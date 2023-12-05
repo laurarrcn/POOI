2add607dbc03f3f1815840cdd8f4362430d87efe
laurarrcn <125471455+laurarrcn@users.noreply.github.com>
2023-12-05 15:50:50 -0300

    Create Cliente.java

---
+++ ATIV GRUPO EMPRESARIAL/Cliente.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.main;
+import java.util.*;
+
+public class Cliente {
+    public String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    
+}
