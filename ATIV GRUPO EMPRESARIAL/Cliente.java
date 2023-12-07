96cd8695a6aa407f683d4b4ab04676e40ab482a3
laurarrcn <125471455+laurarrcn@users.noreply.github.com>
2023-12-07 15:38:29 -0300

    Update Cliente.java

--- ATIV GRUPO EMPRESARIAL/Cliente.java
+++ ATIV GRUPO EMPRESARIAL/Cliente.java
@@ -1,12 +1,13 @@
 
 package com.mycompany.main;
-import java.util.*;
 
-public class Cliente {
-    public String nome;
 
-    public Cliente(String nome) {
-        this.nome = nome;
+public abstract class Cliente {
+    public String usuario;
+
+    public Cliente(String usuario) {
+        this.usuario = usuario;
     }
     
+    public abstract void imprimir();
 }
