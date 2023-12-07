a30f9ce85555183209accb652d3b40881d8f7958
laurarrcn <125471455+laurarrcn@users.noreply.github.com>
2023-12-07 15:38:03 -0300

    Update Main.java

--- ATIV GRUPO EMPRESARIAL/Main.java
+++ ATIV GRUPO EMPRESARIAL/Main.java
@@ -1,11 +1,24 @@
 
-
 package com.mycompany.main;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+      Administradora adm  = new Administradora(0.1, true, "Nome", "111", "1111");
+      adm.calcularAtivosFinanceiros();
+      
+      Juridica j1 = new Juridica("usuario1", "Nome1", "111111");
+      j1.imprimir();
+      
+      Fisica f1 = new Fisica("usuario2", "nome2", "222222");
+      f1.imprimir();
+      
+     
+      
+      
+       
     }
-}
+
+   
