f8aeebe67144cf77f592126d97d3265dd8c7dc02
laurarrcn <125471455+laurarrcn@users.noreply.github.com>
2023-12-07 15:39:47 -0300

    Update Comercio.java

--- ATIV GRUPO EMPRESARIAL/Comercio.java
+++ ATIV GRUPO EMPRESARIAL/Comercio.java
@@ -2,13 +2,13 @@
 package com.mycompany.main;
 import java.util.*;
 
-public class Comercio extends Juridica{
+public class Comercio extends Juridica {
     private String ramoOperacao;
-    List<Venda> vendas;
+List<Venda>vendas;
+ 
     
-
-    public Comercio(String ramoOperacao, String nomeFantasia, String cnpj, String nome) {
-        super(nomeFantasia, cnpj, nome);
+    public Comercio(String ramoOperacao, String nomeFantasia, String cnpj, String usuario) {
+        super(nomeFantasia, cnpj, usuario);
         this.ramoOperacao = ramoOperacao;
         vendas = new ArrayList<>();
     }
@@ -20,12 +20,22 @@ public class Comercio extends Juridica{
     public void setRamoOperacao(String ramoOperacao) {
         this.ramoOperacao = ramoOperacao;
     }
-
-    public void adicionarVendas(Venda v){
-        vendas.add(v);
+    
+  
+    
+    @Override
+    public void imprimir(){
+        System.out.println("Ramo operacao: "+ramoOperacao);
     }
-
     
+    public void criarVenda(int id, double valor){
+         Venda v1 = new Venda(1, 1);
+    }
     
-    
+    public void addVenda(Venda v1){
+      vendas.add(v1);
+      v1.setComercio(this);
+      
+      
+    }
 }
